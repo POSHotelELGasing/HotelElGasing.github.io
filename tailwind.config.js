81e13026695b3349c7df2a0804f01ea24ce3b308
Dany <muhammaddeny000@gmail.com>
2024-12-09 10:23:10 +0700

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Sesuaikan dengan struktur proyekmu
+    "./**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
